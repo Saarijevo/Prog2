798488e318e189a8dc4e01c5e71c6d6ded96f2de
Max Johansson Saarijarvi <majo6530@atterbom.it.uu.se>
2022-10-21 10:46:34 +0200

    new files

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3.9
 
 from person import Person
-
+import matplotlib.pyplot as plt
+import numpy as np
 def main():
-	f = Person(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
+    f = Person(7)
+   # print(f.get())
+   # print(f.fib())
+
+    t2_py = []
+    t2_numba = []
+    fib2 = np.arange(20,31)
+    for i in fib2:
+        f.set(i)
+        t2_py += [f.fib()[3]]
+        t2_numba += [f.fib()[4]]
+    plt.plot(fib2, t2_py)
+    plt.plot(fib2, t2_numba)
+    plt.savefig('fib2')
 
 if __name__ == '__main__':
 	main()
--- MA4/person.py
+++ MA4/person.py
@@ -1,22 +1,50 @@
 """ Python interface to the C++ Person class """
 import ctypes
 lib = ctypes.cdll.LoadLibrary('./libperson.so')
+from numba import njit
+from time import perf_counter as pc
 
 class Person(object):
-	def __init__(self, age):
-		lib.Person_new.argtypes = [ctypes.c_int]
-		lib.Person_new.restype = ctypes.c_void_p
-		lib.Person_get.argtypes = [ctypes.c_void_p]
-		lib.Person_get.restype = ctypes.c_int
-		lib.Person_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
-		lib.Person_delete.argtypes = [ctypes.c_void_p]
-		self.obj = lib.Person_new(age)
+    def __init__(self, age):
+        lib.Person_new.argtypes = [ctypes.c_int]
+        lib.Person_new.restype = ctypes.c_void_p
+        lib.Person_get.argtypes = [ctypes.c_void_p]
+        lib.Person_get.restype = ctypes.c_int
+        lib.Person_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
+        lib.Person_delete.argtypes = [ctypes.c_void_p]
+        self.obj = lib.Person_new(age)
+    
+    
+    def get(self):
+        return lib.Person_get(self.obj)
 
-	def get(self):
-		return lib.Person_get(self.obj)
-
-	def set(self, age):
-		lib.Person_set(self.obj, age)
+    def fib(self):
+        start = pc()
+        def fib_py(n):
+            if n <= 1:
+                return n
+            else:
+                return(fib_py(n-1) + fib_py(n-2))
+        end = pc()
+        t_py = end - start
         
-	def __del__(self):
-		return lib.Person_delete(self.obj)
+        start = pc()
+        
+        @njit
+        def fib_numba(n):
+            if n  <= 1:
+                return n
+            else:
+                 return(fib_numba(n-1) + fib_numba(n-2))
+        end = pc()
+        t_numba = end - start
+
+        return [fib_py(self.get()), fib_numba(self.get()), 0, t_py, t_numba]
+
+    def set(self, age):
+        lib.Person_set(self.obj, age)
+
+    def __del__(self):
+        return lib.Person_delete(self.obj)
+
+
